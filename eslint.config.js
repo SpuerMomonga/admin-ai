ad7bdec71cd3bd00090ba1e6d0aeb32d51f37f29
SpuerMomonga <1638829841@qq.com>
2026-01-23 17:58:09 +0800

    chore: Update .gitignore, enhance Playwright config, and improve ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,36 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  svelte: true,
-  typescript: true,
-  formatters: true,
-  ignores: [
-    '**/node_modules/**',
-    '**/dist/**',
-    '**/build/**',
-    '**/coverage/**',
-    '**/.svelte-kit/**',
-    '**/.output/**',
-    '**/.vercel/**',
-    '**/.netlify/**',
-  ],
-})
+export default antfu([
+  {
+    svelte: true,
+    typescript: true,
+    formatters: true,
+  },
+  {
+    rules: {
+      'n/prefer-global/process': 'off',
+      'no-control-regex': 'off',
+      'toml/padding-line-between-pairs': 'off',
+      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/brace-style': 'off',
+      'no-redeclare': 'off',
+      'pnpm/json-enforce-catalog': 'off',
+      '@typescript-eslint/no-redeclare': 'off',
+      'ts/no-unsafe-function-type': 'off',
+      'ts/no-namespace': 'off',
+      'unicorn/error-message': 'off',
+      'pnpm/json-prefer-workspace-settings': 'off',
+      'no-restricted-syntax': 'off',
+    },
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+      '**/.svelte-kit/**',
+      '**/.output/**',
+      '**/.vercel/**',
+      '**/.netlify/**',
+    ],
+  },
+])
